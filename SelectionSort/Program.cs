55d6bb70b8de5e59e111b60bdc60006dc5fefa5d
Anton Pashko <anton.pashko.ol@gmail.com>
2017-09-06 10:53:28 +0300

    ready

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+
+        public static void SelectionSort(ref int [] array)
+        {
+            int min, swap;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                min = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[min] > array[j])
+                    {
+                        swap = array[min];
+                        array[min] = array[j];
+                        array[j] = swap;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[] { 5,15,15,24,11,55,4,1,74,157,23 };
+
+            SelectionSort(ref array);
+
+            foreach (int elem in array)
+            {
+                Console.WriteLine(elem);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
